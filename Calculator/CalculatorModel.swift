098cd0e498cce8d41d292c47b71c49ab39adef27
蘇健豪 <jenhausu@icloud.com>
2017-01-24 11:20:08 +0800

    init CalculatorModel

---
+++ Calculator/CalculatorModel.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorModel.swift
+//  Calculator
+//
+//  Created by 蘇健豪1 on 2017/1/24.
+//  Copyright © 2017年 Oyster. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorModel {
+    
+}
